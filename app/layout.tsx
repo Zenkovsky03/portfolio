58003b2d64bc06fc58dd0e6e2bde13b6a701757a
Matthew <mateuszzaniewski1@gmail.com>
2024-03-05 18:22:34 +0100

    add: simply header with motion

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-        <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+        <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px]`}>
             <div
                 className={
                 "bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem]" +
@@ -31,7 +32,7 @@ export default function RootLayout({
                     " xl:left-[-15rem]" +
                     " 2xl:left-[-5rem]"}>
             </div>
-
+            <Header />
             {children}
         </body>
     </html>
