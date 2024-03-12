dda1beeb88f8a5c6fc0c7768d75ca100e89e5826
Matthew <mateuszzaniewski1@gmail.com>
2024-03-13 00:09:56 +0100

    edit: intro section

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,15 +1,19 @@
 "use client";
 
+import Link from "next/link";
+import {BsArrowRight, BsLinkedin} from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
 import React from "react";
-import Image from 'next/image'
+import Image from 'next/image';
 import { motion } from "framer-motion";
+import {FaGithubSquare} from "react-icons/fa";
 
-export default function Intro(){
+export default function Intro() {
     return (
         <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
             <div className="flex items-center justify-center">
                 <div className="relative">
-                    <motion.div initial={{opacity: 0, scale: 0}} animate={{opacity: 1, scale: 1}} transition={{ type: "tween", duration: 0.2 }}>
+                    <motion.div initial={{opacity: 0, scale: 0}} animate={{opacity: 1, scale: 1}} transition={{type: "tween", duration: 0.2}}>
                         <Image
                             src="/profile_image.jpg"
                             alt="Matthew Zanesky Image"
@@ -22,12 +26,20 @@ export default function Intro(){
                     </motion.div>
                 </div>
             </div>
-            <motion.p className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl" initial={{opacity: 0, y: 100}} animate={{opacity: 1, y: 0}}>
+            <motion.p className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+                      initial={{opacity: 0, y: 100}} animate={{opacity: 1, y: 0}}>
                 <span className="font-bold">Hello, I'm Mateusz.</span> I'm a{" "}
                 <span className="font-bold">backend developer</span> I enjoy
                 learning and creating<span className="italic"> webpage</span>. My focus is{" "}
                 <span className="underline">Python (Django & DRF)</span>.
             </motion.p>
+            <motion.div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+                        initial={{opacity: 0, y: 100}} animate={{opacity: 1, y: 0}} transition={{delay: 0.1,}}>
+                <Link href="#contact" className="bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full">Contact me here <BsArrowRight/></Link>
+                <a href="" className="bg-white px-7 py-3 flex items-center gap-2 rounded-full">Download CV <HiDownload/></a>
+                <a href="" className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full"><BsLinkedin/></a>
+                <a href="" className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem]"><FaGithubSquare/></a>
+            </motion.div>
         </section>
     )
 }
