82c4e35dbd002d69b14e5d2d2de07ae908d955e2
Matthew <mateuszzaniewski1@gmail.com>
2024-05-20 10:25:33 +0200

    add: basic tracking of navigation and components

--- components/intro.tsx
+++ components/intro.tsx
@@ -10,7 +10,7 @@ import {FaGithubSquare} from "react-icons/fa";
 
 export default function Intro() {
     return (
-        <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+        <section id="home" className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
             <div className="flex items-center justify-center">
                 <div className="relative">
                     <motion.div initial={{opacity: 0, scale: 0}} animate={{opacity: 1, scale: 1}} transition={{type: "tween", duration: 0.2}}>
