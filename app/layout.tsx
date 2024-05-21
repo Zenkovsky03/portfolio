3545f70f3946b85b3719f94ee29857c49db1bac7
Matthew <mateuszzaniewski1@gmail.com>
2024-05-21 10:51:44 +0200

    add: active-section-context to provide header section using context

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import ActiveSectionContextProvider from "@/context/active-section-context";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -31,8 +32,10 @@ export default function RootLayout({
                     " xl:left-[-15rem]" +
                     " 2xl:left-[-5rem]"}>
             </div>
-            <Header />
-            {children}
+            <ActiveSectionContextProvider>
+                <Header />
+                {children}
+            </ActiveSectionContextProvider>
         </body>
     </html>
   );
--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -1,8 +1,32 @@
-import React, {useState} from 'react'
+"use client"
+import React, {useState, createContext, useContext} from 'react'
+import {links} from "@/lib/data";
 
-export default function ActiveSectionContextProvider(){
-    const [activeSection, setActiveSection] = useState("Home");
+type SectionName = (typeof links)[number]["name"]
+
+type ActiveSectionContextProviderProps = {
+    children: React.ReactNode
+}
+type ActiveSectionContextType = {
+    activeSection: SectionName;
+    setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+}
+
+export const ActiveSectionContext = createContext<ActiveSectionContextType | null>(null) //default null kiedy uzyjemy tego poza obszarem contextu
+export default function ActiveSectionContextProvider({ children }: ActiveSectionContextProviderProps){
+    const [activeSection, setActiveSection] = useState<SectionName>("Home");
     return(
-        <div></div>
-    )
+        <ActiveSectionContext.Provider value={{
+            activeSection,
+            setActiveSection
+        }}>{children}</ActiveSectionContext.Provider>
+    );
+}
+
+export function useActiveSectionContext(){
+    const context = useContext(ActiveSectionContext)
+    if (context === null){
+        throw new Error("useActiveSectionContext must be used within an ActiveSectionContextProvider");
+    }
+    return context;
 }
