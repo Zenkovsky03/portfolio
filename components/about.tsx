1d5f412ddcc1dfc89c4782c106c2ca83cde2aa6a
Matthew <mateuszzaniewski1@gmail.com>
2024-05-22 21:49:13 +0200

    refactorig and cleaning code, delete dummy import and use hooks

--- components/about.tsx
+++ components/about.tsx
@@ -4,17 +4,19 @@ import SectionHeading from "@/components/section-heading";
 import {motion} from "framer-motion";
 import {useInView} from "react-intersection-observer";
 import {useActiveSectionContext} from "@/context/active-section-context";
+import {useSectionInView} from "@/lib/hooks";
 
 export default function About(){
-    const {ref, inView} = useInView({
-        threshold: 0.75,
-    })
-    const {setActiveSection, timeOfLastClick} =  useActiveSectionContext()
-    useEffect(() => {
-        if(inView && Date.now() - timeOfLastClick > 1000){
-            setActiveSection("About");
-        }
-    }, [inView, setActiveSection, timeOfLastClick])
+    const { ref } = useSectionInView("About")
+    // const {ref, inView} = useInView({
+    //     threshold: 0.75,
+    // })
+    // const {setActiveSection, timeOfLastClick} =  useActiveSectionContext()
+    // useEffect(() => {
+    //     if(inView && Date.now() - timeOfLastClick > 1000){
+    //         setActiveSection("About");
+    //     }
+    // }, [inView, setActiveSection, timeOfLastClick])
 
     return(
         <motion.section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28" initial={{ opacity:0, y:100 }} animate={{ opacity:1, y:0 }} transition={{ delay: 0.175 }} id="about" ref={ref} >
--- components/projects.tsx
+++ components/projects.tsx
@@ -5,17 +5,19 @@ import {projectsData} from "@/lib/data";
 import Project from "@/components/project";
 import {useInView} from "react-intersection-observer";
 import {useActiveSectionContext} from "@/context/active-section-context";
+import {useSectionInView} from "@/lib/hooks";
 
 export default function Projects(){
-    const {ref, inView} = useInView({
-        threshold: 0.5,
-    })
-    const {setActiveSection, timeOfLastClick} =  useActiveSectionContext()
-    useEffect(() => {
-        if(inView && Date.now() - timeOfLastClick > 1000){
-            setActiveSection("Projects");
-        }
-    }, [inView, setActiveSection, timeOfLastClick])
+    const { ref } = useSectionInView("Projects", 0.5)
+    // const {ref, inView} = useInView({
+    //     threshold: 0.5,
+    // })
+    // const {setActiveSection, timeOfLastClick} =  useActiveSectionContext()
+    // useEffect(() => {
+    //     if(inView && Date.now() - timeOfLastClick > 1000){
+    //         setActiveSection("Projects");
+    //     }
+    // }, [inView, setActiveSection, timeOfLastClick])
     return (
         <section id="projects" className="scroll-mt-28" ref={ref}>
             <SectionHeading>My Projects</SectionHeading>
